76956ecc55e015626fc4a11e70e0f1a4f8897177
minibug1021 <21983581+minibug1021@users.noreply.github.com>
2026-06-06 19:53:47 +0200

    of all the things to overlook

--- entralinked/utility/db_manager.py
+++ entralinked/utility/db_manager.py
@@ -158,4 +158,5 @@ class DBManager:
             self._local.conn.close()
             self._local.conn = None
 
+Path(ROOT_DIR / "save_data").mkdir(exist_ok=True)
 db = DBManager(Path(ROOT_DIR / "save_data" / "pokemon_saves.db"))
